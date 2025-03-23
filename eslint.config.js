20ae3ba6e21ffba9e1b67e74c5f21249ed15f168
AidenDonohoe <149549898+AidenDonohoe@users.noreply.github.com>
2025-03-23 02:10:03 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -15,6 +16,7 @@ export default tseslint.config(
       parser: tseslint.parser,
     },
     plugins: {
+      react, // Add React plugin
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -24,6 +26,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // Add React-specific rules
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
     },
   }
 );
