2bc593aff4d3a8e11b0d10638495293226754bf5
AidenDonohoe <aidendonohoe@ufl.edu>
2025-04-19 03:44:27 -0400

    removed eslint warning

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { User } from '../types';
 import  { getCurrentUser } from '../services/api';
