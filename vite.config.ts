f8b725b46339f426d38292cf79d6f0af09887969
AidenDonohoe <149549898+AidenDonohoe@users.noreply.github.com>
2025-04-19 03:22:00 -0400

    Sentry removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "university-of-florida-ti",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "cardmatch-cen3031",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   optimizeDeps: {
     exclude: ['lucide-react'],
