640a5d9016774d8e005218f232ccd28d34df56ed
AidenDonohoe <149549898+AidenDonohoe@users.noreply.github.com>
2025-04-19 04:19:24 -0400

    import from correct file location

--- __tests__/Login.test.tsx
+++ __tests__/Login.test.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Login from '../pages/LoginPage';
+import Login from '../src/pages/LoginPage';
 import { MemoryRouter } from 'react-router-dom';
 import axios from 'axios';
 
