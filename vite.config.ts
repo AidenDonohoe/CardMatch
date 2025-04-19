94301f647f1ed3827e244e293bff3d0f284a8505
AidenDonohoe <149549898+AidenDonohoe@users.noreply.github.com>
2025-04-19 04:21:10 -0400

    changed to add resolve and setupFiles to test: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,12 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true, 
+    setupFiles: './scr/test/setup.ts',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
     coverage: {
       provider: 'v8', 
       reporter: ['text', 'json', 'html'], 
